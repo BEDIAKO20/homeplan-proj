1f88d90dfa8e2d63371e7cf6ae74f694e03bc378
Kofi Bediako-Mensah Boadi <boadikofi466@gmail.com>
2023-09-21 06:38:21 +0000

    All the code

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,32 +1,17 @@
 import React from "react";
 import Carouel from "../../ui/carousel/Carousel";
 import Carddi from "../../ui/CardDl/Carddi";
-import homeplanData from "../../assets/Data/homeplans";
-import { Container,Row,Col } from "react-bootstrap";
+// import homeplanData from "../../assets/Data/homeplans";
+// import { Container, Row, Col } from "react-bootstrap";
 
 function Home() {
   return (
     <div>
       <Carouel />
-      <Container>
-        <Row>
-          <Col md={3} lg={4}>
-          {homeplanData.map((item) => (
-        <Carddi key={item.id} item={item} />
-      ))}
-          </Col>
-        </Row>
-      </Container>
-      {/* <div className="container-md ">
-        <div className="row">
-          <div className="col-md-3">
-          {homeplanData.map((item) => (
-        <Carddi key={item.id} item={item} />
-      ))}
-          </div>
-        </div>
-      </div> */}
-     
+
+      <div>
+        <Carddi />
+      </div>
     </div>
   );
 }
--- src/ui/CardDl/Carddi.js
+++ src/ui/CardDl/Carddi.js
@@ -1,34 +1,40 @@
-import React from 'react'
-import '../CardDl/card.css'
+import React from "react";
+import "../CardDl/card.css";
 // import homeplan  from '../../assets/homeplans1.jpg'
-// import homeplanData from "../../assets/Data/homeplans"
- import { NavLink } from 'react-router-dom'
+import homeplanData from "../../assets/Data/homeplans";
+import { NavLink } from "react-router-dom";
 
-
-
-
-
-
-function Carddi(props) {
-const {imgUrl,plantry,aboutplan,id} = props.item
+function Carddi() {
+  // const {imgUrl,plantry,aboutplan,id} = props.item
   return (
     <div>
-    <div className=' me-5   card-container'>
-            <div class="card-wrapper" >
-              <div class="card-top ">
-                <img class="image" src={imgUrl} alt='plantry' />
-              </div>
-              <div class="card-bottom">
-                <span class="top-text  text-sm-end">{plantry}</span><br/>
-                <span class="bottom-text text-sm-end">{aboutplan}</span><br/>
-              <NavLink to={`/plandetail/${id}`}  className="button ">Get it</NavLink>
-                {/* <button class="button text-sm-end" to>Join Us</button> */}
-              </div>
+    <div className="container">
+  <div className="row">
+    {homeplanData.map((item) => (
+      <div key={item.id} className="col-md-3 col-12">
+        <div className="card-container">
+          <div className="card-wrapper">
+            <div className="card-top">
+              <img className="image" src={item.imgUrl} alt="plantry" />
             </div>
-         
-    </div>
+            <div className="card-bottom">
+              <span className="top-text text-sm-end">{item.plantry}</span>
+              <br />
+              <span className="bottom-text text-sm-end">{item.aboutplan}</span>
+              <br />
+              <NavLink to={`/plandetail/${item.id}`} className="button">
+                Get it
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    ))}
   </div>
-  )
+</div>
+
+    </div>
+  );
 }
 
-export default Carddi
+export default Carddi;
